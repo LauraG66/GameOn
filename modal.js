233500084e42219a6b1df10dda1c60722b9e92c1
Laura Grenier <laura.grenier6@gmail.com>
2024-12-13 15:55:50 +0100

    maj code 5 erreurs

--- modal.js
+++ modal.js
@@ -7,10 +7,13 @@ function editNav() {
   }
 }
 
+document.getElementById("icon-nav").addEventListener("click", editNav)
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalBody = document.querySelector(".modal-body");
+const modalValidate = document.querySelector(".modal-vaidate");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,17 +23,100 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+
+// VALIDATION AUTOMATIQUE
+
+const firstnameInput = document.querySelector("#first");
+firstnameInput.addEventListener("change", (event) =>  {
+  const errorDiv = document.querySelector("#firstError");
+  if (event.target.value.trim().length < 2)
+  {
+    errorDiv.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+  } else {
+    errorDiv.innerHTML= "";
+  }
+});
+
+
+const lastnameInput = document.querySelector("#last");
+lastnameInput.addEventListener("change", (event) =>  {
+  const errorDiv = document.querySelector("#lastError");
+  if (event.target.value.trim().length < 2)
+  {
+    errorDiv.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+  } else {
+    errorDiv.innerHTML= "";
+  }
+});
+
+const emailAddressInput = document.querySelector("#email");
+emailAddressInput.addEventListener("change", (event) => {
+  const regex = new RegExp("^[A-Za-z0-9.\-_]+@[A-Za-z0-9.\-_]+\.[A-Za-z]+");
+  const errorDiv = document.querySelector('#emailError');
+  if (!regex.test(event.target.value)) {
+    errorDiv.innerHTML = "Veuillez entrer votre adresse email.";
+  } else {
+    errorDiv.innerHTML= "";
+  }
+});
+
+const birthdateInput = document.querySelector("#birthdate");
+birthdateInput.addEventListener("change", (event) =>  {
+  const regex = new RegExp("^[0-9]{4}-[0-9]{2}-[0-9]{2}$");
+  const errorDiv = document.querySelector("#birthdateError");
+  if (!regex.test(event.target.value)) {
+    errorDiv.innerHTML = "Veuillez entrer votre date de naissance.";
+  } else {
+    errorDiv.innerHTML= "";
+  }
+  console.log(regex.test(event.target.value)) 
+  console.log(event.target.value)
+});
+
+const quantityInput = document.querySelector("#quantity");
+quantityInput.addEventListener("change", (event)=> {
+  const errorDiv = document.querySelector("#quantityError");
+  if (event.target.value.length == 0) {
+     errorDiv.innerHTML = "Veuillez entrer une valeur.";
+  } else {
+    errorDiv.innerHTML= "";
+  }
+});
+
+const locationInput = document.querySelectorAll("input[name='location']");
+locationInput.forEach(element => (
+  element.addEventListener("change", (event) => {
+    const errorDiv = document.querySelector("#locationError"); 
+    errorDiv.innerHTML= "";
+  })
+))
+
+const checkbox1Input = document.querySelector("#checkbox1");
+checkbox1Input.addEventListener("click", (event) => {
+  const errorDiv = document.querySelector("#checkbox1Error");
+  if (!event.target.checked) {
+    errorDiv.innerHTML = "Veuillez accepter les conditions.";
+  } else {
+    errorDiv.innerHTML= "";
+  }
+});
+
 const submitBtn = document.querySelector(".btn-submit");
 submitBtn.addEventListener("click", confirmModal);
+
+// FONCTION COUPE EXECUTION SI ERREUR
 
 function confirmModal(evt) 
 { 
   evt.preventDefault()
   let error = false
 
+
+  // DEUXIEME VERIFICATION PAR ENVOI DU FORMULAIRE
+  
   const firstnameInput = document.querySelector("#first");
   const firstErrorDiv = document.querySelector("#firstError");
-  if (firstnameInput.value.length < 2)
+  if (firstnameInput.value.trim().length < 2)
   {
     firstErrorDiv.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
     error = true 
@@ -40,7 +126,7 @@ function confirmModal(evt)
   
   const lastnameInput = document.querySelector("#last");
   const lastErrorDiv = document.querySelector("#lastError");
-  if (lastnameInput.value.length < 2)
+  if (lastnameInput.value.trim().length < 2)
   {
     lastErrorDiv.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
     error = true 
@@ -95,12 +181,23 @@ function confirmModal(evt)
     checkbox1Error.innerHTML = "";
   }
 
+  // SI FORMULAIRE VALIDE POPUP REUSSITE
   if (!error) {
-    const modalText = `<div> Merci ! Votre réservation a été reçue.</div>`;
+    const modalText = `<div id="confirmMessage">
+      <div> Merci ! Votre réservation a été reçue.</div>
+      <input
+      id="closeButton"
+      class="modal-btn"
+      type="submit"
+      class="button"
+      value="Fermer"/> 
+    </div> `;
     modalBody.innerHTML= modalText;
+    document.getElementById("closeButton").addEventListener("click", closeModal)
   }
 }
 
+// FERMETURE DE LA FENETRE POPUP
 const close = document.querySelector(".close");
 close.addEventListener("click", closeModal);
 function closeModal()
@@ -109,78 +206,3 @@ function closeModal()
 
 }
 
-const firstnameInput = document.querySelector("#first");
-firstnameInput.addEventListener("change", (event) =>  {
-  const errorDiv = document.querySelector("#firstError");
-  if (event.target.value.length < 2)
-  {
-    errorDiv.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
-  } else {
-    errorDiv.innerHTML= "";
-  }
-  console.log(event.target.value.length)
-});
-
-const lastnameInput = document.querySelector("#last");
-lastnameInput.addEventListener("change", (event) =>  {
-  const errorDiv = document.querySelector("#lastError");
-  if (event.target.value.length < 2)
-  {
-    errorDiv.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-  } else {
-    errorDiv.innerHTML= "";
-  }
-  console.log(event.target.value.length)
-});
-
-const emailAddressInput = document.querySelector("#email");
-emailAddressInput.addEventListener("change", (event) => {
-  const regex = new RegExp("^[A-Za-z0-9.\-_]+@[A-Za-z0-9.\-_]+\.[A-Za-z]+");
-  const errorDiv = document.querySelector('#emailError');
-  if (!regex.test(event.target.value)) {
-    errorDiv.innerHTML = "Veuillez entrer votre adresse email.";
-  } else {
-    errorDiv.innerHTML= "";
-  }
-});
-
-const birthdateInput = document.querySelector("#birthdate");
-birthdateInput.addEventListener("change", (event) =>  {
-  const regex = new RegExp("^[0-9]{4}-[0-9]{2}-[0-9]{2}$");
-  const errorDiv = document.querySelector("#birthdateError");
-  if (!regex.test(event.target.value)) {
-    errorDiv.innerHTML = "Veuillez entrer votre date de naissance.";
-  } else {
-    errorDiv.innerHTML= "";
-  }
-  console.log(regex.test(event.target.value)) 
-  console.log(event.target.value)
-});
-
-const quantityInput = document.querySelector("#quantity");
-quantityInput.addEventListener("change", (event)=> {
-  const errorDiv = document.querySelector("#quantityError");
-  if (event.target.value.length == 0) {
-     errorDiv.innerHTML = "Veuillez entrer une valeur.";
-  } else {
-    errorDiv.innerHTML= "";
-  }
-});
-
-const locationInput = document.querySelectorAll("input[name='location']");
-locationInput.forEach(element => (
-  element.addEventListener("change", (event) => {
-    const errorDiv = document.querySelector("#locationError"); 
-    errorDiv.innerHTML= "";
-  })
-))
-
-const checkbox1Input = document.querySelector("#checkbox1");
-checkbox1Input.addEventListener("click", (event) => {
-  const errorDiv = document.querySelector("#checkbox1Error");
-  if (!event.target.checked) {
-    errorDiv.innerHTML = "Veuillez accepter les conditions.";
-  } else {
-    errorDiv.innerHTML= "";
-  }
-});
